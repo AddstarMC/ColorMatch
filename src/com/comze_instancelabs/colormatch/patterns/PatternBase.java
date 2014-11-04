c6aa0219d049576a9d62d0aa52ae427b6e58ff5f
Schmoller <steven.schmoll@gmail.com>
2014-11-04 11:27:30 +1100

    Added PatternBase and SquaresPattern: * All patterns will inherit from PatternBase   - It provides methods for using the pattern including placing it * SquaresPattern creates patterns of varying amounts of 4x4 squares

---
+++ src/com/comze_instancelabs/colormatch/patterns/PatternBase.java
@@ -0,0 +1,157 @@
+package com.comze_instancelabs.colormatch.patterns;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.DyeColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.material.MaterialData;
+import org.bukkit.material.Wool;
+
+import com.comze_instancelabs.colormatch.Main;
+
+public abstract class PatternBase {
+	private HashSet<PatternGroup> groups;
+	
+	public abstract int getWidth();
+	public abstract int getHeight();
+	
+	/**
+	 * Gets the pixel at the coords. Must return null if the coords are out of range
+	 */
+	public abstract PatternPixel getPixel(int x, int y);
+	
+	public final void processPattern() {
+		HashSet<PatternPixel> visited = new HashSet<PatternPixel>();
+		groups = new HashSet<PatternGroup>();
+		
+		for (int x = 0; x < getWidth(); ++x) {
+			for (int y = 0; y < getHeight(); ++y) {
+				PatternPixel pixel = getPixel(x, y);
+				
+				if (pixel.material.getItemType() == Material.AIR || visited.contains(pixel))
+					continue;
+				
+				// Do a flood fill to find all matching blocks
+				PatternGroup group = new PatternGroup();
+				LinkedList<PatternPixel> floodQueue = new LinkedList<PatternPixel>();
+				floodQueue.add(pixel);
+				
+				while(!floodQueue.isEmpty()) {
+					PatternPixel floodPixel = floodQueue.pop();
+					
+					if (!visited.add(pixel))
+						continue;
+					
+					group.getPixels().add(pixel);
+					
+					// Pixel to left
+					if (isMatch(getPixel(x-1, y), floodPixel))
+						floodQueue.add(getPixel(x-1, y));
+					
+					// Pixel to right
+					if (isMatch(getPixel(x+1, y), floodPixel))
+						floodQueue.add(getPixel(x+1, y));
+					
+					// Pixel to top
+					if (isMatch(getPixel(x, y-1), floodPixel))
+						floodQueue.add(getPixel(x, y-1));
+					
+					// Pixel to bottom
+					if (isMatch(getPixel(x, y+1), floodPixel))
+						floodQueue.add(getPixel(x, y+1));
+				}
+				
+				groups.add(group);
+			}
+		}
+	}
+	
+	private boolean isMatch(PatternPixel pixel, PatternPixel current) {
+		if (pixel == null)
+			return false;
+		
+		return current.material.equals(pixel.material);
+	}
+	
+	public final void placeAt(Location location, Material material, List<Block> modified, Random random) {
+		if (groups == null)
+			processPattern();
+		
+		for (PatternGroup group : groups) {
+			DyeColor colour = Main.colors.get(random.nextInt(Main.colors.size()));
+			Wool groupMat = new Wool(material);
+			groupMat.setColor(colour);
+			
+			group.placeAt(location, groupMat, modified);
+		}
+	}
+	
+	/**
+	 * Represents a block in the pattern
+	 */
+	public static class PatternPixel {
+		public final int offsetX;
+		public final int offsetY;
+		public final MaterialData material;
+		
+		public PatternPixel(int offsetX, int offsetY, MaterialData material) {
+			this.offsetX = offsetX;
+			this.offsetY = offsetY;
+			this.material = material;
+		}
+		
+		public Location getLocation(Location origin) {
+			return new Location(origin.getWorld(), origin.getBlockX() + offsetX, origin.getBlockY(), origin.getBlockZ() + offsetY);
+		}
+		
+		@Override
+		public int hashCode() {
+			return 37 ^ offsetX | 17 ^ offsetY << 16;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof PatternPixel))
+				return false;
+			
+			PatternPixel other = (PatternPixel)obj;
+			
+			return other.offsetX == offsetX && other.offsetY == offsetY && other.material.equals(material);
+		}
+	}
+	
+	/**
+	 * Represents a group of pixels
+	 */
+	public static class PatternGroup {
+		private final List<PatternPixel> pixels;
+		
+		public PatternGroup() {
+			pixels = new ArrayList<PatternPixel>();
+		}
+		
+		public List<PatternPixel> getPixels() {
+			return pixels;
+		}
+		
+		public void placeAt(Location origin, MaterialData material, List<Block> modified) {
+			for (PatternPixel pixel : pixels) {
+				Location pixelLocation = pixel.getLocation(origin);
+				
+				Block block = pixelLocation.getBlock();
+				block.setType(material.getItemType());
+				BlockState state = block.getState();
+				state.setData(material);
+				
+				modified.add(block);
+			}
+		}
+	}
+}
---
+++ src/com/comze_instancelabs/colormatch/patterns/SquaresPattern.java
@@ -0,0 +1,69 @@
+package com.comze_instancelabs.colormatch.patterns;
+
+import org.bukkit.Material;
+import org.bukkit.material.MaterialData;
+
+/**
+ * A pattern consisting of squares each 4x4 blocks
+ */
+public class SquaresPattern extends PatternBase {
+	private static final int squareSize = 4;
+	
+	private int width;
+	private int height;
+	
+	private PatternPixel[] pixels;
+	
+	public SquaresPattern(int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+		generatePattern();
+	}
+	
+	private void generatePattern() {
+		pixels = new PatternPixel[getWidth() * getHeight()];
+		
+		// The material doesnt matter as long as the squares are separated
+		MaterialData even = new MaterialData(Material.STONE);
+		MaterialData odd = new MaterialData(Material.DIRT);
+		
+		// Each square
+		for (int x = 0; x < width; ++x) {
+			for (int y = 0; y < height; ++y) {
+				MaterialData mat;
+				if (x + y % 2 == 0)
+					mat = even;
+				else
+					mat = odd;
+				
+				// Each pixel in square
+				for (int px = 0; px < squareSize; ++px) {
+					for (int py = 0; py < squareSize; ++py) {
+						PatternPixel pixel = new PatternPixel(x * squareSize + px, y * squareSize + py, mat);
+						pixels[pixel.offsetX + pixel.offsetY * getWidth()] = pixel;
+					}
+				}
+			}
+		}
+	}
+	
+	@Override
+	public int getWidth() {
+		return width * squareSize;
+	}
+
+	@Override
+	public int getHeight() {
+		return height * squareSize;
+	}
+
+	@Override
+	public PatternPixel getPixel(int x, int y) {
+		if (x < 0 || y < 0 || x >= getWidth() || y >= getHeight())
+			return null;
+		
+		return pixels[x + y * getWidth()];
+	}
+
+}
